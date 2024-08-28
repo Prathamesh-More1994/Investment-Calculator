428ae9f51e0bc3764f671dfe87440a3fc048efdd
“Prathamesh <prathamesh.more9405@gmail.com>
2024-08-28 18:17:56 +0530

    Modules

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,9 @@
 import { Component, output, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import type { InvestmentInput } from '../investment-input';
 import { InvestmentService } from '../investment-results/investment.service';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
 })
